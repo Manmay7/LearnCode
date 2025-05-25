b01ab3dc76f209d3fea43afe03c6062431c35940
Manmay7 <manmaymaheshwari744@gmail.com>
2025-05-25 17:07:01 +0530

    Pass Code code

--- my-app/src/Pages/PassCodeChallenge.jsx
+++ my-app/src/Pages/PassCodeChallenge.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react'
-
+import "./PassCode.css";
 function PassCodeChallenge() {
     const num=[0,1,2,3,4,5,6,7,8,9];
-    const handleClick=()=>{
-    }
-    const [passcode,setpass]=useState("0000");
+    const [passcode,setPass]=useState("0000");
+    const [,setCount]=useState(0);
+    function replaceCharAt(str, index, replacement) {
+      if (index < 0 || index >= str.length) return str; // index out of range
+      return str.substring(0, index) + replacement + str.substring(index + 1);
+    }    
+    const handleClick = (digit) => {
+      setCount(prevCount => {
+        let newCount = prevCount + 1;
+        let newPass = replaceCharAt(passcode, prevCount, digit);
+        
+        if (prevCount === 4) {
+          setPass("0000");
+          return 0;
+        } else {
+          setPass(newPass);
+          return newCount;
+        }
+      });
+    };
     // const passcode="0000";
   return (<>
         <div className="numbers-grid">
+          <div className='number-button'> 
         {num.map((num) => (
           <button className="button" key={num} onClick={() => handleClick(num)}>
             {num}
@@ -17,6 +35,8 @@ function PassCodeChallenge() {
         <div>
         password is {passcode}
         </div>
+        </div>
+        
         </>
   )
 }
